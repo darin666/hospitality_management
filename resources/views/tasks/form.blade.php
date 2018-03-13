ef435370ca65ea01f88b7377b5459ea270baa711
unknown <daveadjei@yahoo.co.uk>
2018-03-13 14:48:55 +0100

    created task controller and task blade

---
+++ resources/views/tasks/form.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    Display the form for tasks
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/apartments', 'ApartmentController@index');
+Route::get('/tasks', 'TaskController@index');
