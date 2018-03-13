02590adba31e9c9ebc7993f5cf7dce1b900c1cc3
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-03-13 11:11:25 +0100

    register page - skeleton done

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,8 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'tel' => 'required|string|max:20',
+            'img_link' => 'required|string',
         ]);
     }
 
@@ -63,10 +65,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+//        dd($data);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'tel' => $data['tel'],
+            'img_link' => $data['img_link']
         ]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email','tel', 'password','img_link'
     ];
 
     /**
