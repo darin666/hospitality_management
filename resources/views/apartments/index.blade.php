471f74cc9bf759de8f3dd9b5cb0533e76db58b49
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-03-13 11:45:21 +0100

    Add routing, link, view and controller for apartments

---
+++ resources/views/apartments/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    Display all the flats we have. 
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        <li><a class="nav-link" href="{{ action('ApartmentController@index') }}">Apartments</a></li>
 
                     </ul>
 
