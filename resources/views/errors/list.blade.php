e7b82fb8ebd55c389502c01a4e76de9f45fa103e
Darin <dario.princ@gmail.com>
2018-03-18 23:39:43 +0100

    list of validation errors moved to views/errors

---
+++ resources/views/errors/list.blade.php
@@ -0,0 +1,8 @@
+<!-- error message if form validation fails -->
+@if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
