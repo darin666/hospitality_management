c95e9a929495a4aaa88ca1b2c68063700c4c2471
Darin <dario.princ@gmail.com>
2018-03-18 23:38:12 +0100

    forms show blade updated

--- resources/views/forms/show.blade.php
+++ resources/views/forms/show.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<h4>{{ $form->firstname }} {{ $form->lastname }}, {{ $form->nationality }}</h4>
-<h5>Arrived: {{ $form->checkInDate }}, Leaving: {{ $form->checkOutDate }}</h5>
-<h6>Apartment: {{ $form->apartment_id }}</h6>
+    <h4>{{ $form->firstname }} {{ $form->lastname }}, {{ $form->nationality }}</h4>
+    <h5>Arrived: {{ $form->checkInDate }}, Leaving: {{ $form->checkOutDate }}</h5>
+    <h6>Apartment: {{ $form->apartment_id }}</h6>
     <br/>
 
     <div>Date of birth: {{ $form->dob }}</div>
