71853c168166fefd4c5034edd31d01d045d3d0c9
Darin <dario.princ@gmail.com>
2018-03-18 20:44:48 +0100

    error message when form validation fails

--- resources/views/forms/create.blade.php
+++ resources/views/forms/create.blade.php
@@ -61,6 +61,13 @@
 
     {!! Form::close() !!}
 
-
+    <!-- error message if validation fails -->
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
 @endsection
