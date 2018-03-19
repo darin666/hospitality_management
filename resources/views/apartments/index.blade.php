4dc32b6f007611c1f342120cee6e3fbdd6c6cf01
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-03-19 16:13:18 +0100

    no msg

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -4,11 +4,11 @@
     <div class="container">
     Display all the flats we have.
         @foreach($apartments as $apartment)
-            <div class="btn btn-success">
 
-                <a href="{{ route('show apartment',['id'=>$apartment->id]) }}">{{$apartment->name}}</a>
 
-            </div>
+                <a class="btn btn-success" href="{{ route('show apartment',['id'=>$apartment->id]) }}">{{$apartment->name}}</a>
+
+
         @endforeach
     <a class="btn btn-primary" href="{{ action('ApartmentController@create') }}">Add a new apartment</a>
 
