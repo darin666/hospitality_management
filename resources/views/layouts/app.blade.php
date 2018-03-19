04a61f4f15d130ac20a5cf99de6630ca2d5039c3
Darin <dario.princ@gmail.com>
2018-03-19 14:28:58 +0100

    forms added to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,7 @@
                     <ul class="navbar-nav mr-auto">
                         <li><a class="nav-link" href="{{ action('ApartmentController@index') }}">Apartments</a></li>
                         <li><a class="nav-link" href="{{ action('TaskController@index') }}">Tasks</a></li>
+                        <li><a class="nav-link" href="{{ action('FormController@index') }}">Forms</a></li>
 
                     </ul>
 
