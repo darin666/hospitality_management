0e5822cce53f5201828c749b8ec2e3c9c4f4ccd0
Darin <dario.princ@gmail.com>
2018-03-20 17:14:12 +0100

    all issues page genesis

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Task;
 use App\Apartment;
+use App\User;
 use App\Http\Requests\CreateTaskRequest;
 
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -20,7 +20,25 @@
     <br><a class="btn btn-primary" href="{{ action('TaskController@create') }}">Create a new task</a><br>
     </div>
 
+    <hr/>
 
+    <h3>My tasks</h3>
+
+        @foreach($tasks as $newTask)<br>
+            @if(Auth::user()->id == $newTask->user_id)
+                <a class="btn btn-danger" href="{{ route('show task',['id'=>$newTask->id]) }}">{{$newTask->name}}
+                <br>
+                {{$newTask->description}}
+                <br>
+                @foreach($shownApartments as $shownApartment)
+                    @if($newTask->apartment_id == $shownApartment->id)
+                    {{ $shownApartment->name }}
+                    @endif
+                @endforeach
+                </a>
+                <br>
+            @endif
+        @endforeach
 
 @endsection
 
