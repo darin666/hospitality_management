ec9fb0331484e44f04bd766240f3a4783e01fe3c
Darin <dario.princ@gmail.com>
2018-03-20 15:40:22 +0100

    validating a new task, every user can add tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Task;
 use App\Apartment;
+use App\Http\Requests\CreateTaskRequest;
 
 class TaskController extends Controller
 {
@@ -43,12 +44,12 @@ class TaskController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateTaskRequest $request)
     {
         $newTask = new Task();
         $newTask->user_id = \Auth::id();
         $newTask->name = $request->get('name');
-        $newTask->raisedBy_id = Auth::id();
+        $newTask->raisedBy_id = \Auth::id();
         $newTask->apartment_id = $request->get('apartment_id');
         $newTask->status_id = $request->get('status_id');
         $newTask->category_id = $request->get('category_id');
---
+++ app/Http/Requests/CreateTaskRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'apartment_id' => 'required',
+            'status_id',
+            'name',
+            'category_id',
+            'statusChange_id',
+            'description' => 'required',
+            'img_link',
+        ];
+    }
+}
