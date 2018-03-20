307910a0bcb98a5ae3abc1456339a5606f728eaf
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-03-20 11:05:19 +0100

    Pass only 1 apartment id when creating new reservations

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -6,7 +6,6 @@
         @foreach($reservations as $reservation)
             <div class="btn btn-primary">{{$reservation->id}}</div>
         @endforeach
-        <a class="btn btn-primary" href="{{ action('ReservationController@create') }}">Add a new reservation</a>
 
     </div>
 
