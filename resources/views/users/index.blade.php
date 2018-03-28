61fead4c6bbe6df7bf151c12e1b6f141846be2d5
unknown <daveadjei@yahoo.co.uk>
2018-03-28 18:03:56 +0200

    display all users and my details seperately

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,36 +1,56 @@
-@extends('layouts.app')
+@extends('layouts.app') @section ('content')
 
-@section ('content')
+
+
 
 <h2>Users</h2>
+<div class="row">
 
-@foreach ($users as $user)
+    <div class="col-sm-4 mb-5">
 
-<users>
-<h5>
-@can('edit')
-<a href="{{action('UserController@show', [$user->id])}}"> {{$user->name}} {{$user->lastname}}</a>
-@endcan
+        @foreach ($users as $user)
 
-</h5>
+        <users>
+            <h5>
+                @can('edit')
+                <a href="{{action('UserController@show', [$user->id])}}"> {{$user->name}} {{$user->lastname}}</a>
+                @endcan
 
-@can('edit-emp')
-<a href="{{action('UserController@show', [$user->id])}}"> {{$user->name}} {{$user->lastname}}</a>
-@endcan
-
-</h5>
+            </h5>
 
 
-
-<h5>Email: {{$user->email}}</h5>
-<h5>Tel: {{$user->tel}}</h5>
-<h5>User role id: {{$user->role_id}}</h5>
+            <h5>Name: {{$user->name}} {{$user->lastname}}</h5>
+            <h5>Email: {{$user->email}}</h5>
+            <h5>Tel: {{$user->tel}}</h5>
+            <h5>User role id: {{$user->role_id}}</h5>
 
 
 
-</users><br>
+        </users>
+        <br> @endforeach
+    </div>
+
+    <hr>
+    <div class="col-sm-4 mb-5">
+
+        <h3>My details</h3>
+
+        @foreach ($users as $user) @if(Auth::user()->id == $user->id) @can('edit-emp')
+
+        <a href="{{action('UserController@show', [$user->id])}}"> {{ $user->name }}</a>
+        <h5>Email: {{$user->email}}</h5>
+        <h5>Tel: {{$user->tel}}</h5>
+        @endcan @can('edit')
+
+        <a href="{{action('UserController@show', [$user->id])}}"> {{ $user->name }}</a>
+        <h5>Email: {{$user->email}}</h5>
+        <h5>Tel: {{$user->tel}}</h5>
+        @endcan @endif @endforeach
+    </div>
+</div>
 
 
-@endforeach
+
+
 
 @endsection
