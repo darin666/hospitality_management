9c49670627b6a6d67bb39f405ec6f867a4db9a7a
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-03-29 17:28:09 +0200

    no msg

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = \App\User::all();
+        $users = User::all();
         return view ('users.index', compact('users'));
     }
 
